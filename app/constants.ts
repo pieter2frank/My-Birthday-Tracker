39c3e3e2656807430914642bd2c4efdf402e29e6
pieter2frank <pieterfrank@vanboven.org>
2025-11-19 17:09:00 +0100

    Extra info for IOS

--- app/constants.ts
+++ app/constants.ts
@@ -10,7 +10,7 @@ export const K_PERSON_IDS = 'person_notif_ids'; // map personId->notifId
 // ====== Free tier & RevenueCat ======
 export const FREE_LIMIT: number = 20;
 export const ENTITLEMENT_ID = 'pro';
-export const RC_API_KEY_IOS = 'appl_xxxxxxxxxxxxxxxxxxxxxxxx';
+export const RC_API_KEY_IOS = 'appl_uWziFqKOiDZkDhYjoleAvinYjkl';
 export const RC_API_KEY_ANDROID = 'goog_QxJqDyJvGIlTJAfIfrCxYMwAyjd';
 export const RC_API_KEY = Platform.OS === 'ios' ? RC_API_KEY_IOS : RC_API_KEY_ANDROID;
 
